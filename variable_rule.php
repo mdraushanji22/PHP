b3594713b1dcd94a30a1e46078e8d307eff2830b
mdraushan <mdraushanji22@gmail.com>
2025-12-28 23:24:59 +0530

    form-submit

---
+++ variable_rule.php
@@ -0,0 +1,16 @@
+<!-- Variables Rule:- -->
+<?php
+
+$name = "Raushan <br>";
+$Name = "Jilani <br>";
+$NAME = "peter <br>";
+
+echo $name;
+echo $Name;
+echo $NAME;
+
+$a = $b = 30;
+echo $a;
+
+
+?>
