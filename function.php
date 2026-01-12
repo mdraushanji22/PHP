54952bba37fe0b5d7aa7b3524cccfc06e27713f0
mdraushan <mdraushanji22@gmail.com>
2026-01-12 16:09:19 +0530

    php

--- function.php
+++ function.php
@@ -1,9 +1,20 @@
 <h1 style="color: green;">Functions in php</h1>
 <?php
 
-function test()
+function dummy()
 {
 
-    echo "Functions Call";
+    echo "<h1>Functions Test</h1>";
 }
+
+function test()
+{
+    dummy();
+    echo "Functions Call";
+    echo "<br>";
+    echo "<hr>";
+}
+test();
+test();
+test();
 test();
