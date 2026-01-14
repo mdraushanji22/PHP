117617acfac53d52972c179f2fb8271cca86d585
mdraushan <mdraushanji22@gmail.com>
2026-01-14 14:43:25 +0530

    php

---
+++ foreach-loop.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $users = ["peter", "bruce", "harry", "tony"];
+
+    foreach ($users as $y) {
+
+        echo "$y";
+        echo "<br>";
+    }
+    ?>
+</body>
+
+</html>
