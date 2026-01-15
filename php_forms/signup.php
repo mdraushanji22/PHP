fd9455f6609da12feb1999622d70ad885220c34e
mdraushan <mdraushanji22@gmail.com>
2026-01-15 23:25:58 +0530

    php

---
+++ php_forms/signup.php
@@ -0,0 +1,11 @@
+<?php
+
+if ($_POST) {
+
+    echo "Name is: " . $_POST['name'];
+    echo "<br>";
+    echo "Email is: " . $_POST['email'];
+    echo "<br>";
+    echo "Password is: " . $_POST['password'];
+    echo "<br>";
+}
