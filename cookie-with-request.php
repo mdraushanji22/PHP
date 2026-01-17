560b0d8659b0264486e393e6cfe0eed3b9e73316
mdraushan <mdraushanji22@gmail.com>
2026-01-17 23:36:09 +0530

    php

---
+++ cookie-with-request.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>cookie with request</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <input type="text" name="user" placeholder="Enter username">
+        <br>
+        <br>
+        <button name="button" value="set">Set cookies</button>
+        <br>
+        <br>
+        <button name="button" value="display">Display cookie</button>
+        <br>
+        <br>
+        <button name="button" value="delete">Delete cookie</button>
+    </form>
+</body>
+
+</html>
+<?php
+if (isset($_POST['button'])) {
+    if ($_POST['button'] == "set") {
+
+        $val = $_POST['user'];
+        echo $val;
+    }
+}
+
+?>
