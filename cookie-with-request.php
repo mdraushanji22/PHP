01cc9ea95dc4f725fed1009f64f7d6db11579ec7
mdraushan <mdraushanji22@gmail.com>
2026-01-18 21:53:13 +0530

    php

--- cookie-with-request.php
+++ cookie-with-request.php
@@ -26,7 +26,20 @@ if (isset($_POST['button'])) {
     if ($_POST['button'] == "set") {
 
         $val = $_POST['user'];
-        setcookie("user", $val); 
+        setcookie("user", $val);
+        echo "Cookie is set";
+    }
+    if ($_POST['button'] == "display") {
+        if (isset($_COOKIE['user'])) {
+            echo "<br>";
+            echo $_COOKIE['user'];
+        }
+    }
+    if ($_POST['button'] == "delete") {
+        if (isset($_COOKIE['user'])) {
+            echo "<br>";
+            setcookie('user', '', -1);
+        }
     }
 }
 
