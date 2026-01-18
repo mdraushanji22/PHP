c686dae102123243cbdd6cbb01d2647f07c87763
mdraushan <mdraushanji22@gmail.com>
2026-01-18 23:20:10 +0530

    php

--- write-file.php
+++ write-file.php
@@ -1,10 +1,16 @@
 <?php
 
-$fileName = "dummy.txt";
+$fileName = "files/dummy.txt";
 $content = "This is dummy file";
-$file = fopen($fileName, "w") or die("Unable to file");
+
+// If folder does NOT exist, create it
+if (!file_exists("files")) {
+    mkdir("files", 0777, true);
+}
+
+$file = fopen($fileName, "w") or die("Unable to open file");
 
 fwrite($file, $content);
 fclose($file);
 
-echo "created file";
+echo "Created file successfully!";
