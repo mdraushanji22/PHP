d43d8ff10b7ebd8648738d00e95935a15bc9a62d
mdraushan <mdraushanji22@gmail.com>
2026-01-19 22:37:58 +0530

    basic_project

---
+++ php_forms/website/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="header">
+        <h3>LOGO</h3>
+        <ul>
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+
+        </ul>
+
+    </div>
+</body>
+
+</html>
