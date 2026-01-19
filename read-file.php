71057d6f5f8e192f0dd546b35fdc4368de7de263
mdraushan <mdraushanji22@gmail.com>
2026-01-19 10:29:24 +0530

    php

--- read-file.php
+++ read-file.php
@@ -1,5 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Read File</title>
+</head>
+
+<body>
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="file" name="files">
+        <br>
+        <br>
+        <button>Read file</button>
+    </form>
+</body>
+
+</html>
+
+
+
 <?php
-$fileName = "files/dummy.txt";
-$myfile = fopen($fileName, "r") or die("Unable to read file");
-echo fread($myfile, filesize($fileName));
-fclose($myfile);
+if (isset($_FILES['files'])) {
+    $fileName = $_FILES['files']['tmp_name'];
+    $myfile = fopen($fileName, "r") or die("Unable to read file");
+    echo fread($myfile, filesize($fileName));
+    fclose($myfile);
+}
