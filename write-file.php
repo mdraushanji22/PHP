ebab3a5668ba48fc2785841c926aec7d39b90076
mdraushan <mdraushanji22@gmail.com>
2026-01-19 08:23:17 +0530

    php

--- write-file.php
+++ write-file.php
@@ -1,16 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Multiple file created</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <input type="text" name="filename" placeholder="Enter file name">
+        <br>
+        <br>
+        <textarea name="content" placeholder="Enter content"></textarea>
+        <br>
+        <br>
+        <button>Create files</button>
+    </form>
+</body>
+
+</html>
+
 <?php
 
-$fileName = "files/dummy.txt";
-$content = "This is dummy file";
+if (isset($_POST['filename'])) {
 
-// If folder does NOT exist, create it
-if (!file_exists("files")) {
-    mkdir("files", 0777, true);
+    $fileName = "files/" . $_POST['filename'];
+    $content = $_POST['content'];
+
+    // If folder does NOT exist, create it
+    if (!file_exists("files")) {
+        mkdir("files", 0777, true);
+    }
+
+    $file = fopen($fileName, "w") or die("Unable to open file");
+
+    fwrite($file, $content);
+    fclose($file);
+
+    echo "Created file successfully!";
 }
-
-$file = fopen($fileName, "w") or die("Unable to open file");
-
-fwrite($file, $content);
-fclose($file);
-
-echo "Created file successfully!";
